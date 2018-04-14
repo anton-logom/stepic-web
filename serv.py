893437faf6a5fc9980868217f235c4f4e782e08b
anton-logom <anton-logom@yndex.ru>
2018-04-15 00:57:06 +0300

    2

--- serv.py
+++ serv.py
@@ -7,9 +7,11 @@ server_socket.listen(10)
 while True:                                                                     
         client_socket, remote_address = server_socket.accept()                  
         try:                                                                    
-                request = client_socket.recv(1024)                              
-                client_socket.send(request.upper())                             
-                client_socket.close()                                           
+                data = client_socket.recv(1024)                              
+                client_socket.send(request.upper())
+				if not data or data == "close": client_socket.close()                            
+				client_socket.send(data)  
+                client_socket.close()                                         
         except:                                                                 
                 pass                                                            
 server_socket.close()                                              
