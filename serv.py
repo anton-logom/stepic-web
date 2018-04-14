963a42fde3e7c28336852dae3c3a8cc8f1590c7c
anton-logom <anton-logom@yndex.ru>
2018-04-15 00:50:59 +0300

    1

---
+++ serv.py
@@ -0,0 +1,15 @@
+import socket                                                                   
+ 
+server_socket = socket.socket()                                                 
+server_socket.bind(('0.0.0.0', 2222))                                           
+server_socket.listen(10)                                                        
+    
+while True:                                                                     
+        client_socket, remote_address = server_socket.accept()                  
+        try:                                                                    
+                request = client_socket.recv(1024)                              
+                client_socket.send(request.upper())                             
+                client_socket.close()                                           
+        except:                                                                 
+                pass                                                            
+server_socket.close()                                              
