6e8c842d3a2368f621622e83a90ba139161eba3f
anton-logom <anton-logom@yndex.ru>
2018-04-16 23:26:58 +0300

    eeeeeeeee

--- web/init.sh
+++ web/init.sh
@@ -1,3 +1,3 @@
-﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+﻿/usr/bin/ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 rm -rf /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
