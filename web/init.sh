8d7eb7ebc44e25e1a6f68af5cfbcc9d3a235f409
anton-logom <anton-logom@yndex.ru>
2018-04-18 02:06:13 +0300

    444

--- web/init.sh
+++ web/init.sh
@@ -1,7 +1,7 @@
 ﻿# 
 
 rm -rf /etc/nginx/sites-enabled/default
-ln -sf /home/box/web/etc/nginx.conf   /etc/nginx/sites-enabled/test.conf
+ln -sf /home/box/web/etc/nginx.conf   /etc/nginx/sites-enabled/default
 /etc/init.d/nginx restart
 ln -sf /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
