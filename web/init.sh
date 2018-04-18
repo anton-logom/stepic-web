1f306a62ac757ebf6c3072486067d8ce0599136f
anton-logom <anton-logom@yndex.ru>
2018-04-18 12:02:47 +0300

    1235

--- web/init.sh
+++ web/init.sh
@@ -4,4 +4,5 @@ rm -rf /etc/nginx/sites-enabled/default
 ln -sf /home/box/web/etc/nginx.conf   /etc/nginx/sites-enabled/test.conf
 /etc/init.d/nginx restart
 ln -sf /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+
 sudo /etc/init.d/gunicorn restart
